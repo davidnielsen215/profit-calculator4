af60b853274d51d81326e467b78708488168d490
David Nielsen <davidnielsen215@gmail.com>
2020-01-27 11:00:19 -0700

    updated button styles

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -14,31 +14,45 @@ export default class LandingPage extends Component {
         
         return (
             
-            <Spring 
-                from={{ opacity: 0}}
-                to={{ opacity: 1}}
-                config={{ duration: 1000}}
-            >
-            { props => (
-                <div style={props}>
+            
 
                     <div className='maincont'>
                         <div className='container1'>
+                        <Spring 
+                        from={{ opacity: 0}}
+                        to={{ opacity: 1}}
+                        config={{ duration: 1500}}
+                        >
+                        { props => (
+                        <div style={props}>
                             <img className='logo' src={Logo} alt=""/>
                             <img className='landinglogo' src={EntireLogo} alt=""/>
+                            </div>
+                            )}
+                        </Spring>
                         </div>
                         <div className="container">
+
+                        <Spring 
+                        from={{ opacity: 0}}
+                        to={{ opacity: 1}}
+                        config={{ duration: 1700}}
+                        >
+                        { props => (
+                        <div style={props}>
                             <h1 className='title'>PROFIT CALCULATOR</h1>
                             <h1 className='question'><b>What's your number?</b></h1>
                             <h3 className='statement'><i>Find out how much extra profit you'll make with us</i></h3>
                             <h1 className='statement2'>Double Your Profit!</h1>
                             <button onClick={this.continue}
                             className='button'>B E G I N</button>
+
+                            </div>
+                            )}
+                        </Spring>
                         </div>
                     </div>
-                </div>
-                    )}
-            </Spring>
+                
             
         )
     }
